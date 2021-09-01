9ff6fb1c3ff84ed595cfbf4ec4e31ecd3961c0c9
alireza <alireza.jhd2000@gmail.com>
2021-09-01 13:18:12 +0430

    Fix a bug in ExamFactory

--- database/factories/ExamFactory.php
+++ database/factories/ExamFactory.php
@@ -23,15 +23,13 @@ class ExamFactory extends Factory
     public function definition()
     {
         $confirmation_required = (bool)rand(0,1);
-        $password = $confirmation_required ? bcrypt('password') : null;
         $start = Carbon::now()->addDays(rand(1,25))->addHours(rand(1,24));
-        $end = $start->add(2, 'hours');
+        $end = Carbon::make($start)->addHours(2);
         return [
             'name' => $this->faker->sentence(),
             'confirmation_required' => $confirmation_required,
-            'password' => $password,
-            'start' => $start->format('YYYY-MM-DD hh:mm:ss'),
-            'end' => $end->format('YYYY-MM-DD hh:mm:ss'),
+            'start' => $start->format('Y-m-d H:i:s'),
+            'end' => $end->format('Y-m-d H:i:s'),
             'total_score' => 100,
         ];
     }
